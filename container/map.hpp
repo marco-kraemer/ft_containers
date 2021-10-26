b8e210bbdeda395665dda57224968fa8c710fde8
Marco Aurelio Santos <marco.k.santos@hotmail.com>
2021-10-26 17:21:56 +0200

    update

--- container/map.hpp
+++ container/map.hpp
@@ -6,7 +6,7 @@
 /*   By: maraurel <maraurel@student.42sp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 11:12:00 by maraurel          #+#    #+#             */
-/*   Updated: 2021/10/26 14:49:29 by maraurel         ###   ########.fr       */
+/*   Updated: 2021/10/26 17:16:20 by maraurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,35 @@ namespace ft
 	class map
 	{
 		public:
-			typedef Key 						key_type;
+			typedef Key 							key_type;
 
-			typedef T						mapped_type;
+			typedef T							mapped_type;
 
-			typedef pair<const key_type, mapped_type>		value_type;
+			typedef pair<const key_type, mapped_type>			value_type;
 
-			typedef Compare						key_compare;
+			typedef Compare							key_compare;
 
-			typedef Alloc						allocator_type;
+			typedef Alloc							allocator_type;
 
-			typedef T&						reference;
+			typedef T&							reference;
 
-			typedef const T&					const_reference;
+			typedef const T&						const_reference;
 
-			typedef T*						pointer;
+			typedef T*							pointer;
 
-			typedef const T*					const_pointer;
+			typedef const T*						const_pointer;
 
-			typedef size_t						size_type;
+			typedef MapIterator<key_type, mapped_type>			iterator;
 
-			typedef BNode<key_type, mapped_type>*			node;
+			typedef const MapIterator<key_type, mapped_type>		const_iterator;
 
-			typedef MapIterator<key_type, mapped_type>		iterator;
+			typedef ReverseMapIterator<key_type, mapped_type>		reverse_iterator;
 
-			typedef ReverseMapIterator<key_type, mapped_type>	reverse_iterator;
+			typedef const ReverseMapIterator<key_type, mapped_type>		const_reverse_iterator;
+		
+			typedef size_t							size_type;
 
-			typedef const MapIterator<key_type, mapped_type>	const_iterator;
-
-			typedef const ReverseMapIterator<key_type, mapped_type>	const_reverse_iterator;
+			typedef BNode<key_type, mapped_type>*				node;
 
 			class value_compare
 			{
